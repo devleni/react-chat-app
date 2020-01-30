9c60793c5ace5e857e34c90e39a85f6d632f1b75
devleni <[devleni@fastmail.com]>
2020-01-30 13:01:56 +0100

    set up project and useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default () => {
+  const [id, setId] = useState("");
+  const [nameINput, setNameInput] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!nameInput) {
+      return alert("Name can't be empty");
+    }
+    setId(name);
+    WebSocket.emit("join", name, room);
+  };
+
+  return id !== "" ? (
+    <div>Hello</div>
+  ) : (
+    <div style={{ textAlign: "center", margin: "30vh auto", width: "70%" }}>
+      <form onSubmit={event => handleSubmit(event)}>
+        <input
+          id="name"
+          onChange={e => setNameInput(e.target.value.trim())}
+          required
+          placedholder="What is your name?"
+          />
+          <br />
+          <input
+            id="room"
+            onChange={e => setRoom(e.target.value.trim())}
+            placeholder="What is your room?"
+          />
+          <br />
+          <button type="submit">Submit</button>
+      </form>
     </div>
   );
-}
-
-export default App;
+};
